9a011d8162a031e243e43a1239679218bdf3d7b8
Prabhjot Singh Assi <prabhjotassi16@gmail.com>
2024-02-18 04:09:10 +0530

    Base GUI added.

---
+++ Interface/gui_base.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+from tkinter import Tk, Canvas, PhotoImage
+from tkinter import *
+from gui import GUI as NewGUI
+
+OUTPUT_PATH = Path(__file__).parent
+ASSETS_PATH = OUTPUT_PATH / Path(r"assets_base")
+
+
+class GUI:
+    def __init__(self):
+        self.window = Tk()
+        self.setup_window()
+        self.create_canvas()
+        self.load_images()
+        self.window.mainloop()
+
+    def setup_window(self):
+        self.window.geometry("1200x700")
+        self.window.configure(bg="#202225")
+        self.window.resizable(False, False)
+
+    def create_canvas(self):
+        self.canvas = Canvas(
+            self.window,
+            bg="#202225",
+            height=700,
+            width=1200,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge",
+        )
+        self.canvas.place(x=0, y=0)
+
+    def load_images(self):
+        self.image_1 = PhotoImage(file=self.relative_to_assets("image_1.png"))
+        self.canvas.create_image(592.0, 350.0, image=self.image_1)
+
+        self.image_2 = PhotoImage(file=self.relative_to_assets("image_2.png"))
+        self.canvas.create_image(601.0, 64.0, image=self.image_2)
+
+        self.image_3 = PhotoImage(file=self.relative_to_assets("image_3.png"))
+        self.button_3 = Button(
+            self.window,
+            image=self.image_3,
+            bg="#2f3235",
+            borderwidth=0,
+            highlightthickness=0,
+        )
+        self.button_3.place(x=-46, y=290.0)
+
+        self.image_4 = PhotoImage(file=self.relative_to_assets("image_4.png"))
+        self.button_4 = Button(
+            self.window,
+            image=self.image_4,
+            bg="#2f3235",
+            borderwidth=0,
+            highlightthickness=0,
+        )
+        self.button_4.place(x=20, y=520.0)
+
+        self.image_5 = PhotoImage(file=self.relative_to_assets("image_5.png"))
+        self.canvas.create_image(90.0, 215.0, image=self.image_5)
+
+    @staticmethod
+    def relative_to_assets(path: str) -> Path:
+        return ASSETS_PATH / Path(path)
+
+
+if __name__ == "__main__":
+    GUI()
